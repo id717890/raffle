0c4d93b0b2badef68307040154ff3a1029151277
YusupovZA <jusupovz@gmail.com>
2018-11-22 13:38:10 +0500

    cors

---
+++ backend.net.core/Sources/Raffle.Api/ViewModels/CredentialsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation.Attributes;
+using Raffle.Api.ViewModels.Validations;
+
+namespace Raffle.Api.ViewModels
+{
+    [Validator(typeof(CredentialsViewModelValidator))]
+    public class CredentialsViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
