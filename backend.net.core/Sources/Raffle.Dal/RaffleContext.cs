78c672e98257d35b281b8344944a0f30e2535fc9
YusupovZA <jusupovz@gmail.com>
2018-11-26 23:27:03 +0500

    add dal and ef

---
+++ backend.net.core/Sources/Raffle.Dal/RaffleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Raffle.Domain.Interface.Entity;
+
+namespace Raffle.Dal
+{
+    public class RaffleContext : IdentityDbContext<ApplicationUser>
+    {
+        public RaffleContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Customer> Customers { get; set; }
+    }
+}
